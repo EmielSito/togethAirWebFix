2eb009116d9136b657579a24df987e6044df7dcb
zeneb es <zayneb.es@gmail.com>
2017-11-09 14:57:09 +0100

    booking backend

--- web/src/main/java/com/realdolmen/group7/service/BookingService.java
+++ web/src/main/java/com/realdolmen/group7/service/BookingService.java
@@ -3,10 +3,13 @@ package com.realdolmen.group7.service;
 import com.realdolmen.group7.domain.search.ClassType;
 import com.realdolmen.group7.domain.search.Seat;
 
+import java.util.Date;
 import java.util.List;
 
 public interface BookingService {
-    public int chooseSeatNumber(String seatNumber);
 
-    public List<Seat> getAvailableSeatByClassType(ClassType type);
+    public void chooseSeatNumber(String seatNumber);
+
+    public List<Seat> getAvailableSeatByPlane(String planeNumber, ClassType type, String departure, String destination,
+                                              Date departureDate, int numberOfSeat);
 }
